64ff0dbfca0f98113ece189b4aa4e36839ec997a
serena7110 <hahnearl.jeon@gmail.com>
2025-05-14 01:20:20 +0000

    1

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,10 +1,14 @@
 import streamlit as st
 import pandas as pd
+import io
 from sklearn.model_selection import train_test_split
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.metrics import accuracy_score
 from sklearn.preprocessing import StandardScaler
-df = pd.read_csv('data/ACE_stat.csv', encoding='utf-8')
+# df = pd.read_csv('data/ACE_stat.csv', encoding='utf-8')
+csv_data = st.secrets["project"]["data1"]
+df = pd.read_csv(io.StringIO(csv_data), encoding = 'utf-8')
+st.dataframe(df)
 
 X = df.iloc[:, :-1].values
 y = df.iloc[:, -1].astype(int).values
@@ -22,7 +26,8 @@ y_pred = knn.predict(X_test)
 
 st.write ("## 당신의 운동 유형은?")
 
-st.write ("다음 질문들에 차례대로 답해보세요")
+st.write ("운동 유형은 ACE 유전자형에 따라 II,ID,DD의 세가지로 나눌 수 있습니다.")
+st.write ("다음 질문에 차례대로 답한 뒤 결과를 확인하세요!")
 
 st.write ("#### Q1 일주일 평균 운동 시간")
 exc = st.text_input("숫자만 작성해주세요 (예:5)", key="q1")
@@ -45,6 +50,8 @@ if st.button('예측하기'):
         user_input_scaled = scaler.transform(user_input)
 
         prediction = knn.predict(user_input_scaled)
+        genotype_labels = {0: "DD", 1: "ID", 2: "II"}
+        selected_genotype = genotype_labels[prediction[0]]
 
         if prediction[0] == 0:
             st.success("당신은 DD형입니다 당신은 다른 사람에 비해 순발력이 필요한 종목에 강할 확률이 높습니다. 그러나 심폐지구력을 요하는 종목에는 어려움을 느낄 수 있습니다. 자신 있는 종목을 꾸준히 이어 나가고, 심폐지구력을 키울 수 있는 수영, 자전거 타기 등의 스포츠를 도전하여 순발력을 키우는 것을 추천합니다!")
@@ -54,7 +61,11 @@ if st.button('예측하기'):
 
         elif prediction[0] == 2:
             st.success("당신은 II형입니다. 당신은 다른 사람에 비해 심폐지구력이 필요한 종목에 강할 확률이 높습니다. 그러나 순발력을 요하는 종목에는 어려움을 느낄 수 있습니다. 자신 있는 종목을 꾸준히 이어 나가고, 순발력을 필요로 하는 배드민턴, 단거리 달리기, 복싱 등의 스포츠를 도전하여 순발력을 키우는 것을 추천합니다!")
-        
+
+        st.write("#### 예측 확률:")
+        for i, label in sorted(genotype_labels.items()):
+            percent = round(prediction_proba[i] * 100, 2)
+            st.write(f"- {label}형: {percent}%")    
 
     except ValueError:
         st.error("입력값이 유효하지 않습니다. 숫자만 입력해 주세요.")
